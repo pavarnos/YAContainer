d24a361faf6ddda5bb0a91a01293e2a4175b7f1d
Steve Pavarno <pavarnos@gmail.com>
2022-02-05 14:18:53 +1300

    fix missing methods in interface

--- src/ContainerInterface.php
+++ src/ContainerInterface.php
@@ -11,5 +11,21 @@ namespace LSS\YAContainer;
 
 interface ContainerInterface
 {
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     * @template T of object
+     * @param class-string<T> $id class name of the entry to look for.
+     * @return object
+     * @phpstan-return T
+     * @throws ContainerException
+     */
+    public function get(string $id): object;
 
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     * @param class-string $id Identifier of the entry to look for.
+     * @return bool
+     */
+    public function has(string $id): bool;
 }
